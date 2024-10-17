54232179675fd5fd5b3eeab9ea002faadc34c82f
huqingyuan314 <hu.qingy@northeastern.edu>
2024-10-17 18:39:12 -0400

    update a3

--- src/Kanbas/Courses/Assignments/Editor.tsx
+++ src/Kanbas/Courses/Assignments/Editor.tsx
@@ -1,23 +1,27 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useParams } from "react-router";
+import * as db from "../../Database";
+import { Link } from 'react-router-dom';
 
 export default function AssignmentEditor() {
+  const { aid, cid } = useParams();
+  const assignments = db.assignments;
+  const assignment = assignments.find((assignment) => assignment._id === aid);
+
     return (
       <div id="wd-assignments-editor" className="container mt-4">
 
         <div className="row mb-4">
           <div className="col">
             <h3>Assignment Name</h3>
-            <input id="wd-name" defaultValue="A1 - ENV + HTML" className="form-control" />
+            <input id="wd-name" defaultValue={assignment && assignment.title} className="form-control" />
           </div>
         </div>
 
         <div className="row mb-4">
           <div className="col">
             <textarea id="wd-description" className="form-control" rows={10}>
-              The assignment is available online. Submit a link to the landing page of your Web application running on Netlify.
-              The landing page should include the following: Your full name and section, Links to each of the lab assignments,
-              Link to the Kanbas application, Links to all relevant source code repositories.
-              The Kanbas application should include a link to navigate back to the landing page.
+            {assignment && assignment.description}
             </textarea>
           </div>
         </div>
@@ -29,7 +33,7 @@ export default function AssignmentEditor() {
             <label htmlFor="wd-points">Points</label>
           </div>
           <div className="col-8 d-flex justify-content-end align-items-center">
-            <input id="wd-points" defaultValue={100} className="form-control" />
+            <input id="wd-points" defaultValue={assignment && assignment.points} className="form-control" />
           </div>
           </div>
 
@@ -121,17 +125,17 @@ export default function AssignmentEditor() {
     <input id="wd-assign-to" defaultValue="Everyone" className="form-control" />
 
     <label htmlFor="wd-due-date" className="col-form-label">Due</label>
-    <input type="date" id="wd-due-date" defaultValue="2024-05-13" className="form-control" />
+    <input type="date" id="wd-due-date" defaultValue={assignment && assignment.dueDate} className="form-control" />
 
 
     <div className="row">
       <div className="col">
         <label htmlFor="wd-available-from" className="col-form-label">Available from</label>
-        <input type="date" id="wd-available-from" defaultValue="2024-05-06" className="form-control" />
+        <input type="date" id="wd-available-from" defaultValue={assignment && assignment.availableDate} className="form-control" />
       </div>
       <div className="col">
         <label htmlFor="wd-available-until" className="col-form-label">Until</label>
-        <input type="date" id="wd-available-until" defaultValue="2024-05-20" className="form-control" />
+        <input type="date" id="wd-available-until" className="form-control" />
       </div>
     </div>
   </div>
@@ -142,8 +146,19 @@ export default function AssignmentEditor() {
 <hr />
 
         <div className="float-end">
-          <button id="wd-cancel-btn" className="btn btn-lg btn-secondary me-2">Cancel</button>
-          <button type="submit" id="wd-save-btn" className="btn btn-lg btn-danger">Save</button>
+
+        <Link id="wd-cancel-btn"
+            to={`/Kanbas/Courses/${cid}/Assignments`}
+            className="btn btn-lg btn-secondary me-2">
+            Cancel </Link>
+
+            <Link id="wd-save-btn" type="submit"
+            to={`/Kanbas/Courses/${cid}/Assignments`}
+            className="btn btn-lg btn-danger">
+            Save </Link>
+
+          {/* <button id="wd-cancel-btn" className="btn btn-lg btn-secondary me-2">Cancel</button>
+          <button type="submit" id="wd-save-btn" className="btn btn-lg btn-danger">Save</button> */}
         </div>
       </div>
     );
--- src/Kanbas/Courses/Assignments/index.tsx
+++ src/Kanbas/Courses/Assignments/index.tsx
@@ -4,127 +4,66 @@ import { FiEdit } from "react-icons/fi";
 import AssignmentsControls from "./AssignmentsControls";
 import AssignmentTypeControlButtons from "./AssignmentTypeControlButtons";
 import AssignmentControlButtons from "./AssignmentControlButtons";
+import { useParams } from "react-router";
+import * as db from "../../Database";
 
 export default function Assignments() {
+  const { cid } = useParams();
+  const assignments = db.assignments;
+
     return (
 <div>
 <AssignmentsControls /><br /><br /><br /><br />
 
 
 
-<ul id="wd-modules" className="list-group rounded-0">
+<ul id="wd-assignments" className="list-group rounded-0">
 
-    <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
-      <div className="wd-title p-3 ps-2 bg-secondary">         
-        <BsGripVertical className="me-2 fs-3" />
-        <RxTriangleDown className="me-2 fs-4"/>
-        ASSIGNMENTS
-        <AssignmentTypeControlButtons />
-         </div>
-      <ul className="wd-lessons list-group rounded-0">
+          <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
+            <div className="wd-title p-3 ps-2 bg-secondary">
+              <BsGripVertical className="me-2 fs-3" />
+              <RxTriangleDown className="me-2 fs-4"/>
+               ASSIGNMENTS 
+               <AssignmentTypeControlButtons />
+            </div>
 
+              <ul className="wd-lessons list-group rounded-0">
+              {assignments
+          .filter((assignment: any) => assignment.course === cid)
+          .map((assignment: any) => (
+                  <li key={assignment._id}
+                      className="wd-lesson list-group-item p-3 ps-1">
 
-      <li className="wd-lesson list-group-item p-3 ps-1">
-        <div className="d-flex align-items-center justify-content-between">
-        <div className="d-flex align-items-center">
-      <BsGripVertical className="me-3 fs-3" />
-      <FiEdit className="me-4 fs-5 text-success" />
+<div className="d-flex align-items-center justify-content-between">
+  <div className="d-flex align-items-center">
+    <BsGripVertical className="me-2 fs-3" />
+    <FiEdit className="me-4 fs-5 text-success" />
+
+    <div>
       <div>
-        <div><a className="wd-assignment-link text-black text-decoration-none"
-              href="#/Kanbas/Courses/1234/Assignments/123">
-              A1 - ENV + HTML
-            </a></div>
-        <div className="text-muted small">
-          <span className="text-danger">Multiple Modules</span> | <b>Not available until</b> May 6 at 12:00am | <br />
-          <b>Due</b> May 13 at 11:59pm | 100 pts
-        </div>
+        <a className="wd-assignment-link text-black text-decoration-none"
+           href={`#/Kanbas/Courses/${cid}/Assignments/${assignment._id}`}>
+          {assignment.title}
+        </a>
       </div>
-    </div>
-    <div className="d-flex align-items-center">
-      <AssignmentControlButtons />
+      <div className="text-muted small">
+        <span className="text-danger">Multiple Modules</span> | <b>Not available until</b> {assignment.availableDate} at {assignment.availableTime} | <br />
+        <b>Due</b> {assignment.dueDate} at {assignment.dueTime} | {assignment.points} pts
+      </div>
     </div>
   </div>
-</li>
 
-<li className="wd-lesson list-group-item p-3 ps-1">
-        <div className="d-flex align-items-center justify-content-between">
-        <div className="d-flex align-items-center">
-      <BsGripVertical className="me-3 fs-3" />
-      <FiEdit className="me-4 fs-5 text-success" />
-      <div>
-        <div><a className="wd-assignment-link text-black text-decoration-none"
-              href="#/Kanbas/Courses/1234/Assignments/123">
-              A2 - CSS + BOOTSTRAP
-            </a></div>
-        <div className="text-muted small">
-          <span className="text-danger">Multiple Modules</span> | <b>Not available until</b> May 13 at 12:00am | <br />
-          <b>Due</b> May 20 at 11:59pm | 100 pts
-        </div>
-      </div>
-    </div>
-    <div className="d-flex align-items-center">
-      <AssignmentControlButtons />
-    </div>
+  <div className="d-flex align-items-center ms-auto">
+    <AssignmentControlButtons />
   </div>
-</li>
+</div>
 
-<li className="wd-lesson list-group-item p-3 ps-1">
-        <div className="d-flex align-items-center justify-content-between">
-        <div className="d-flex align-items-center">
-      <BsGripVertical className="me-3 fs-3" />
-      <FiEdit className="me-4 fs-5 text-success" />
-      <div>
-        <div><a className="wd-assignment-link text-black text-decoration-none"
-              href="#/Kanbas/Courses/1234/Assignments/123">
-              A3 - JAVASCRIPT + REACT
-            </a></div>
-        <div className="text-muted small">
-          <span className="text-danger">Multiple Modules</span> | <b>Not available until</b> May 20 at 12:00am | <br />
-          <b>Due</b> May 27 at 11:59pm | 100 pts
-        </div>
-      </div>
-    </div>
-    <div className="d-flex align-items-center">
-      <AssignmentControlButtons />
-    </div>
-  </div>
-</li>
-
-      </ul>
-    </li>
+                  </li>
+          ))}
+              </ul>
+          </li>
 
 
-    <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
-      <div className="wd-title p-3 ps-2 bg-secondary"> 
-      <BsGripVertical className="me-2 fs-3" />
-      <RxTriangleDown className="me-2 fs-4"/>
-      PAST ASSIGNMENTS
-      <AssignmentTypeControlButtons />
-      </div>
-
-      <li className="wd-lesson list-group-item p-3 ps-1">
-        <div className="d-flex align-items-center justify-content-between">
-        <div className="d-flex align-items-center">
-      <BsGripVertical className="me-3 fs-3" />
-      <FiEdit className="me-4 fs-5 text-success" />
-      <div>
-        <div><a className="wd-assignment-link text-black text-decoration-none"
-              href="#/Kanbas/Courses/1234/Assignments/123">
-              A0 - PREPARATION WORKS
-            </a></div>
-        <div className="text-muted small">
-          <span className="text-danger">Multiple Modules</span> | <b>Closed</b> | <br />
-          <b>Due</b> May 7 at 11:59pm | 100 pts
-        </div>
-      </div>
-    </div>
-    <div className="d-flex align-items-center">
-      <AssignmentControlButtons />
-    </div>
-  </div>
-</li>
-
-    </li>
   </ul>
 
 
