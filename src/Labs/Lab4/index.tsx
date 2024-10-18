54c28fa4ea1c3f17fad4cc8f4bf47e9125612ce7
huqingyuan314 <hu.qingy@northeastern.edu>
2024-10-17 20:38:27 -0400

    update a4-lab

---
+++ src/Labs/Lab4/index.tsx
@@ -0,0 +1,22 @@
+import ClickEvent from "./ClickEvent";
+import ReduxExamples from "./ReduxExamples";
+
+export default function Lab4() {
+    console.log('Hello World!');
+  
+    return (
+      <div id="wd-lab3">
+  <hr />
+  
+        <h3>Lab 4</h3>
+
+        <ClickEvent />
+
+        <ReduxExamples />
+
+
+
+
+</div>
+        );
+        }
