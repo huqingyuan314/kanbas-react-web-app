dac1c15737c93c4de3fb5d2e80c654e611e67695
huqingyuan314 <hu.qingy@northeastern.edu>
2024-10-24 21:04:39 -0400

    update a4-lab2.2

--- src/Labs/Lab4/index.tsx
+++ src/Labs/Lab4/index.tsx
@@ -1,7 +1,15 @@
 import ClickEvent from "./ClickEvent";
 import ReduxExamples from "./ReduxExamples";
+import PassingDataOnEvent from "./PassingDataOnEvent";
+import PassingFunctions from "./PassingFunctions";
+import EventObject from "./EventObject";
 
 export default function Lab4() {
+
+  function sayHello() {
+    alert("Hello");
+  }
+
     console.log('Hello World!');
   
     return (
@@ -11,6 +19,13 @@ export default function Lab4() {
         <h3>Lab 4</h3>
 
         <ClickEvent />
+        <PassingDataOnEvent />
+        <PassingFunctions theFunction={sayHello} />
+        <EventObject />
+
+
+
+
 
         <ReduxExamples />
 
